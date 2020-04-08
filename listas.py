21dde2770cf8c405d2f64bfcce05b668d6bd527a
Margarita <magui@Margarita.local>
2020-04-08 17:46:08 -0300

    Array Listas

---
+++ listas.py
@@ -0,0 +1,22 @@
+#Los array son listas de variables o datos separadas por , y entre [] 
+#Incluso puedo tener otras listas dentro de este
+# listas/array/trupa/diccionario/matrices/arreglos 
+[1,"hello",1.23,True,[1,2,3]]
+
+#puedo ponerle un nombre de vatiable 
+color =["red","green","blue"]
+print (color)
+
+#las ruplas con listas entre () en vez de [] 
+#lo que permiten las tuplas es introducir una funcion 
+numList =list((1,2,3))
+
+#1,2,3 es la tupla y list() la funcion
+print(numList)
+
+#Range es la funcion para crear una lista con un rango
+range(1,10)
+r =list(range(1,10)) #list me da la creacion de lista
+print(r)
+#SUPER IMPORTANTE!
+#no va a tomar el ultimo elemento, range hace la lista desde el primer valor hasta el segundo-1
